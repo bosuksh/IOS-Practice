84ad188343158e5885c88d8744c9600e02c38723
bosuksh <bosuksh@gmail.com>
2018-10-30 01:17:04 +0900

    Firebase Example IOS

---
+++ FirebaseLogin/FirebaseLogin/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  FirebaseLogin
+//
+//  Created by 이상협 on 30/10/2018.
+//  Copyright © 2018 이상협. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import GoogleSignIn
+
+class ViewController: UIViewController, GIDSignInUIDelegate{
+
+    @IBAction func signIn(_ sender: Any) {
+        GIDSignIn.sharedInstance()?.signIn()
+
+    }
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBAction func Login(_ sender: Any) {
+        
+        Auth.auth().createUser(withEmail: email.text!, password: password.text!) { (user, error) in
+            // ...
+            
+            if (error != nil ) {
+                print(error?.localizedDescription)
+                return}
+            
+            
+            let alert  = UIAlertController(title: "알림", message: "회원가입완료", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler: nil))
+            
+                self.present(alert,animated: true, completion: nil)
+            
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        GIDSignIn.sharedInstance()?.uiDelegate = self
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
